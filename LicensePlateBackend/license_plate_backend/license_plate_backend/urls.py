b2fd1f64f79dd35f553eb3257313b50c4cccbfa7
Carvin Edlin <carvin.edlin@koinworks.com>
2019-11-06 22:06:23 +0700

    License Plate Detection API

---
+++ LicensePlateBackend/license_plate_backend/license_plate_backend/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('areas/', include('areas.urls')),
+    path('auth/', include('auth.urls')),
+    path('cameras/', include('cameras.urls')),
+    path('messages/', include('messages.urls')),
+    path('results/', include('results.urls')),
+    path('roles/', include('roles.urls')),
+    path('statuses/', include('statuses.urls')),
+    path('tnkb_colors/', include('tnkb_colors.urls')),
+    path('users/', include('users.urls')),
+    path('vehicles/', include('vehicles.urls')),
+]
